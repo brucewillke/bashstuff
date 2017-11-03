7026af8db72c65446740f6b676a520a41d53a349
brucewillke <brucewillke@users.noreply.github.com>
2017-11-02 17:06:38 -0700

    initial commit

---
+++ copy-ebs-snapshot-to-region.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -xe
+
+aws ec2 describe-snapshots \
+--profile baa \
+--owner-id=$1 \
+--filters Name=tag-key,Values="Name" Name=tag-value,Values="$2" \
+--query 'Snapshots[*].{ID:SnapshotId}' \
+--output text > snaps.txt
+
+kms_key_id="alias/aws/ebs"
+
+for s in $(cat snaps.txt);
+do
+    aws ec2 copy-snapshot \
+    --profile baa \
+    --source-region us-east-1 \
+    --source-snapshot-id $s \
+    --encrypted \
+    --kms-key-id $kms_key_id \
+    --region us-west-2
+done
